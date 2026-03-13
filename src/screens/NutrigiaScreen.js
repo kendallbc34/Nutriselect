09aef46ce963a1af04b9a2fdff000b1328676a98
Kendall <kebeco2412@gmail.com>
2026-03-12 19:34:42 -0600

    Update NutriSelect project

--- src/screens/NutrigiaScreen.js
+++ src/screens/NutrigiaScreen.js
@@ -13,7 +13,7 @@ import {
 import { Image, ImageBackground } from 'react-native';
 import { doc, getDoc } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { db } from '../firebase'; // Ajusta la ruta según tu estructura
+import { db } from '../firebase'; 
 
 const TOGETHER_API_KEY = 'a8333993debf428537cda7a9f6e153af1b21707e05e8c8a971e447648b7c8dc3';
 
